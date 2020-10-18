e3b4784d638ec6b41b7aae1c224d42f1fd8693bb
Chad Yoder <cwyoder13@gmail.com>
2020-10-18 18:47:20 -0500

    corrected models in server routes, added h1 to index.js to make sure server is working

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,14 @@
 const db = require('./database');
 
 //import models
-
+const Item = require('./models/Item');
+const User = require('./models/User');
 
 //model relationships
 
 
 module.exports = {
-  db
+  db,
+  Item,
+  User
 };
