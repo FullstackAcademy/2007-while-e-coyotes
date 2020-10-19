65b3ec197d72385aecbf266751da316c1591be5c
Aminoor Rashid <arashid717@gmail.com>
2020-10-19 00:15:51 -0400

    created seed file and inventory list

---
+++ server/db/inventory/itemsList.js
@@ -0,0 +1,11 @@
+const itemsList = [{
+    name: 'short sword',
+    description: 'This sword is made and blessed by Stanley Lim, and it can kill stuff by stabbing it to death. The holes it produces on the victim disrupt homeostatic behavior leading to hemorrhagic trauma, followed by death.',
+    price: 100,
+    inventory: 1,
+    rarity: 1,
+    itemType: 'sword',
+    itemClass: 'adventurer'
+}]
+
+module.exports = itemsList
