472f20437595f6d7a9fc3f624dfd8de6a7df3c5d
Aminoor Rashid <arashid717@gmail.com>
2020-10-19 12:49:28 -0400

    added rudimentary items to itemsList

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,4 +1,4 @@
-const { red } = require('chalk')
+const { green, red } = require('chalk')
 const {db, Item, User } = require('../db')
 const itemsList = require('./inventory/itemsList')
 
@@ -8,15 +8,18 @@ const random = (limit) => Math.floor(Math.random() * Math.floor(limit))
 //each item can have multiple rarity values, whose range will concatinate one of the following prefixes to the item name 
 const rarity = ['Rusty', 'Normal', 'Magic', 'Rare', 'Legendary']
 
-//need assign function for rarity
+//need 'assign' function for rarity
 
 const seed = async () => {
     try {
         await db.sync({ force: true })
-        await Item.create(itemsList[0])
+        await Item.bulkCreate(itemsList)
+        console.log(green('db successfully seeded'))
     } catch (err) {
         console.log(red(err))
     }
 }
+
+seed()
 
 module.exports = seed
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const { green } = require('chalk') 
 const { db } = require ('../server/db')
-const seed = require ('./db/seed')
+//const seed = require ('./db/seed')
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
-db.sync()
+server.listen(PORT, () => console.log(green('listening on port'), PORT));
+
+//The following code will run the seedfile when the app is first deployed. This is commented out until production.
+/*db.sync()
     .then(() => {
         seed()
         console.log(green('db has been synced'))
         server.listen(PORT, () => console.log(green('listening on port'), PORT));
-    })
+    })*/
