d1fe34db6cf9270879328a366cbe5db815ca3b05
Aminoor Rashid <arashid717@gmail.com>
2020-10-19 17:20:40 -0400

    added random assignments to rarity and rarity classes

--- server/db/inventory/itemsList.js
+++ server/db/inventory/itemsList.js
@@ -3,7 +3,7 @@ const itemsList = [{
     description: 'This small straight sword is widely used, to an extent only matched by the longsword. It causes hemorrhagic trauma to the victim.',
     price: 100,
     inventory: 1,
-    rarity: 1,
+    rarity: 21,
     itemType: 'sword',
     itemClass: 'adventurer'
 },
@@ -12,7 +12,7 @@ const itemsList = [{
     description: 'Widely-used standard straight sword, only matched in ubiquity by the Shortsword. An accessible sword which inflicts consistent regular damage and high slash damage, making it applicable to a variety of situations.',
     price: 150,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'sword',
     itemClass: 'adventurer'
 },
@@ -21,7 +21,7 @@ const itemsList = [{
     description: 'A short sword with a slightly curved blade, formerly used by sailors.',
     price: 120,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'sword',
     itemClass: 'adventurer'
 },
@@ -30,7 +30,7 @@ const itemsList = [{
     description: 'The sword of the great knights. Its blade is wide and thick, and it is wielded in an unusual manner. Causes victims to suffer a horrible death through bisection.',
     price: 200,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'sword',
     itemClass: 'adventurer'
 },
@@ -39,7 +39,7 @@ const itemsList = [{
     description: 'This standard small dagger has only a modest attack but can be jabbed in rapid succession, and is effective in critical hits such as after a parry or when stabbing in the back. With both slash and thrust attacks this dagger is useful in various situations.',
     price: 80,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'dagger',
     itemClass: 'adventurer'
 },
@@ -48,7 +48,7 @@ const itemsList = [{
     description: 'Small bow. Standard projectile weapon. Equip arrows to use.',
     price: 80,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'bow',
     itemClass: 'adventurer'
 },
@@ -57,7 +57,7 @@ const itemsList = [{
     description: 'A standard crossbow. Equip bolts to use. Unlike bows, a crossbow can be held in one hand.',
     price: 100,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'bow',
     itemClass: 'adventurer'
 },
@@ -66,7 +66,7 @@ const itemsList = [{
     description: 'Large bow. Projectile weapon for experienced hunters. Equip arrows to use.',
     price: 120,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'bow',
     itemClass: 'adventurer'
 },
@@ -75,7 +75,7 @@ const itemsList = [{
     description: 'Sorcery-dedicated catalyst that allows the user to harness primordial elemental energy.',
     price: 150,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'magic',
     itemClass: 'adventurer'
 },
@@ -84,7 +84,7 @@ const itemsList = [{
     description: 'A finely-crafted staff, made even more effective by its crystal, the brilliance of which has been polished over time with powerful magic.',
     price: 200,
     inventory: 1,
-    rarity: 1,
+    rarity: 25,
     itemType: 'magic',
     itemClass: 'adventurer'
 }
