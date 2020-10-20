11f0aaae3ba1966cf6b11ee45331518970fd40c8
Ian <ian.k.carson@gmail.com>
2020-10-20 06:36:52 -0400

    added user seed data/seed function for users

---
+++ server/db/userSeedData/userSeed.js
@@ -0,0 +1,63 @@
+module.exports =  [
+    {
+        login : 'BigDaddy',
+        password : 'BigDaddyPass',
+        class: 'villain',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768050179294756864/unknown.png'
+    },
+    {
+        login : 'Mario',
+        password : 'MarioPass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/320416924842590213/768044772648157194/unknown.png'
+    },
+    {
+        login : 'Cuphead',
+        password : 'CupheadPass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/320416924842590213/768045545913581568/unknown.png'
+    },
+    {
+        login : 'Snake',
+        password : 'SnakePass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768050249439641620/unknown.png'
+    },
+    {
+        login : 'Kirby',
+        password : 'KirbyPass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768050101234302978/unknown.png'
+    },
+    {
+        login : 'Sonic',
+        password : 'SonicPass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768047939422781450/unknown.png'
+    },
+    {
+        login : 'Link',
+        password : 'LinkPass',
+        class : 'adventurer',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768048068728979466/unknown.png'
+    },
+    {
+        login : 'Ganondorf',
+        password : 'GanondorfPass',
+        class : 'villain',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768049772199149568/unknown.png'
+    },
+    {
+        login : 'Bowser',
+        password : 'BowserPass',
+        class : 'villain',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768049866529832960/unknown.png'
+    },
+    {
+        login : 'Eggman',
+        password : 'EggmanPass',
+        class : 'villain',
+        userImage : 'https://cdn.discordapp.com/attachments/312072549989351425/768049649231200286/unknown.png'
+    }
+    
+]
