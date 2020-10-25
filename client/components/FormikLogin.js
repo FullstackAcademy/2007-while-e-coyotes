2fd4eb939da4aca019df5654c91a37de1a4e8c97
Ian <ian.k.carson@gmail.com>
2020-10-25 08:46:09 -0400

    deleted unused comments

--- client/components/FormikLogin.js
+++ client/components/FormikLogin.js
@@ -5,16 +5,13 @@ import axios from 'axios'
 const LoginWrapper = Login
 
 export default withFormik({
-    handleSubmit: (values, { setSubmitting }) => {
+    handleSubmit: (values) => {
     
     axios.post('/api/users/login',values,
     {
         withCredentials: true,
         credentials: 'same-origin'
     })
-
-    // Simulates the delay of a real request
-    setTimeout(() => setSubmitting(false), 3 * 1000)
   },
   validationSchema: null,
 })(LoginWrapper)
