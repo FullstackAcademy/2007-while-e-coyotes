35d2ff8318a0f2f4be64bc46ca809aa1df2acd94
Chad Yoder <cwyoder13@gmail.com>
2020-11-07 10:00:04 -0600

    deployment build

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const { green } = require("chalk");
 const { db } = require("../server/db");
-//const seed = require ('./db/seed')
+// const seed = require ('./db/Seed/seed')
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(green("listening on port"), PORT));
 
-db.sync().then(() => {
-  seed();
-  console.log(green("db has been synced"));
-  server.listen(PORT, () => console.log(green("listening on port"), PORT));
-});
+// db.sync().then( async() => {
+//   await seed();
+//   console.log(green("db has been synced"));
+//   server.listen(PORT, () => console.log(green("listening on port"), PORT));
+// });
