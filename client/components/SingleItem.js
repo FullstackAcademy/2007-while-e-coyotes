00181d694007826b99b0f9c13b9314bd1a985115
Chad Yoder <cwyoder13@gmail.com>
2020-11-06 23:46:36 -0600

    started styles for singleItems

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -4,7 +4,7 @@ import { destroyItem } from "../store/itemsReducer";
 import { getItem, destroyReview } from "../store/singleItemReducer";
 import { fetchUser } from "../store/userReducer";
 import { Link } from "react-router-dom";
-import { averageReduce } from "../utils";
+import { averageReduce, itemRarityFinder } from "../utils";
 import { addItem } from "../store/cartReducer";
 
 import { ReviewCard } from "./ReviewCard";
@@ -39,70 +39,76 @@ class SingleItem extends React.Component {
   render() {
     const isAdmin = this.props.user && this.props.user.class === "admin";
     const { singleItem, user } = this.props;
+    const rarityStr = itemRarityFinder(singleItem.rarity);
     return (
       <div id="singleItem">
-        <Link to="/items">Back to Shopping</Link>
-        <div className="itemImg">
-          <img src={singleItem.imageUrl} />
-        </div>
-        <div className="itemDetail">
-          <h1>{singleItem.name}</h1>
-          <p>${singleItem.price}</p>
-          <p className="fancy">{singleItem.description}</p>
-          {this.props ? (
-            <form>
-              <label htmlFor="quantity"> Select Quantity: </label>
-              <input
-                name="quantity"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.quantity}
-              />
-              <button onClick={() => this.handleClick()}>Add to Cart</button>
-            </form>
-          ) : null}
-          {this.state.added ? (
-            <span className="worked">
-              {" "}
-              ✅ Item has been added to your cart!
-            </span>
-          ) : null}
-          <div className="reviews-container">
-            <h2>Reviews:</h2>
-            <div className="review-info">
-              {singleItem.reviews.length === 0 ? (
-                <p>There are no reviews for this item.</p>
-              ) : (
-                <p>
-                  {singleItem.name} has an average rating of{" "}
-                  {averageReduce(singleItem.reviews, "rating").toFixed(1)} stars
-                  from {singleItem.reviews.length} reviews.
-                </p>
-              )}
-              <Link to={`/items/${singleItem.id}/createReview`}>
-                Add a new Review!
-              </Link>
-            </div>
-            {!!singleItem.reviews &&
-              singleItem.reviews.map((review) => {
-                return (
-                  <div key={`review_${review.id}`}>
-                    {review.userId ? (
-                      <ReviewCard
-                        review={review}
-                        user={user}
-                        key={`item_${singleItem.id}_review${review.id}`}
-                      />
-                    ) : null}
-                    {isAdmin ? (
-                      <button onClick={() => this.props.destroyReview(review)}>
-                        Delete Review
-                      </button>
-                    ) : null}
-                  </div>
-                );
-              })}
+        <Link to="/items">Back to shopping</Link>
+        <div className="item-container">
+          <div className="itemImg">
+            <img src={singleItem.imageUrl} />
           </div>
+          <div className="itemDetail">
+            <h1>{singleItem.name}</h1>
+            <p className={`rarity-tag rarity-tag__${rarityStr.slice(0, 3)}`}>
+              {rarityStr.toUpperCase()}
+            </p>
+            <p className="fancy">{singleItem.description}</p>
+            <p className="price">${singleItem.price}</p>
+            {this.props ? (
+              <form>
+                <label htmlFor="quantity"> Select Quantity: </label>
+                <input
+                  name="quantity"
+                  type="text"
+                  onChange={this.handleChange}
+                  value={this.state.quantity}
+                />
+                <button onClick={() => this.handleClick()}>Add to Cart</button>
+              </form>
+            ) : null}
+            {this.state.added ? (
+              <span className="worked">
+                {" "}
+                ✅ Item has been added to your cart!
+              </span>
+            ) : null}
+          </div>
+        </div>
+        <div className="reviews-container">
+          <h2>Reviews:</h2>
+          <div className="review-info">
+            {singleItem.reviews.length === 0 ? (
+              <p>There are no reviews for this item.</p>
+            ) : (
+              <p>
+                {singleItem.name} has an average rating of{" "}
+                {averageReduce(singleItem.reviews, "rating").toFixed(1)} stars
+                from {singleItem.reviews.length} reviews.
+              </p>
+            )}
+            <Link to={`/items/${singleItem.id}/createReview`}>
+              Add a new Review!
+            </Link>
+          </div>
+          {!!singleItem.reviews &&
+            singleItem.reviews.map((review) => {
+              return (
+                <div key={`review_${review.id}`}>
+                  {review.userId ? (
+                    <ReviewCard
+                      review={review}
+                      user={user}
+                      key={`item_${singleItem.id}_review${review.id}`}
+                    />
+                  ) : null}
+                  {isAdmin ? (
+                    <button onClick={() => this.props.destroyReview(review)}>
+                      Delete Review
+                    </button>
+                  ) : null}
+                </div>
+              );
+            })}
         </div>
         {isAdmin ? (
           <div>
