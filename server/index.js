b1dac9b70ca06944b008d07af09c04524b984e40
Chad Yoder <cwyoder13@gmail.com>
2020-11-07 09:52:04 -0600

    fixing seed?

--- server/index.js
+++ server/index.js
@@ -9,10 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(green("listening on port"), PORT));
 
-//The following code will run the seedfile when the app is first deployed. This is commented out until production.
-/*db.sync()
-    .then(() => {
-        seed()
-        console.log(green('db has been synced'))
-        server.listen(PORT, () => console.log(green('listening on port'), PORT));
-    })*/
+db.sync().then(() => {
+  seed();
+  console.log(green("db has been synced"));
+  server.listen(PORT, () => console.log(green("listening on port"), PORT));
+});
